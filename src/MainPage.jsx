1f850a21d2e080b8d13e908dd1705f6e5fc3a9f2
tnirms <rawish49@gmail.com>
2025-03-04 16:32:47 -0800

    Version 5

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -10,7 +10,16 @@ export default function MainPage() {
     //setIngredients is the function used to update this list
 
     const [recipe, setRecipe] = React.useState("")
-    
+    const recipeSection = React.useRef(null) //if using ref to set on a DOM node, its standard pratice to initialize it as null
+    //the purpose of ref is so that we can call scrollIntoView on it, when the recipe loads so that the browser will automatically scroll down
+    //check IngredientsList.jsx where its added
+
+    React.useEffect(() => {
+        if (recipe !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipe]) //rerun whenever recipe changes
+
     async function getRecipe() {
         const recipeMarkdown = await getRecipeFromMistral(ingredients)
         setRecipe(recipeMarkdown)
@@ -21,6 +30,7 @@ export default function MainPage() {
         const newIngredient = formData.get("ingredient") //ingredient is the name of the input in the form
         setIngredients(prevIngredients => [...prevIngredients, newIngredient]) //it is crucial for properly updating the ingredients state
     }
+
     //setIngredients(prevIngredient => [...prevIngredient, newIngredient]) updates the state by adding the new ingredient to the existing list.
 
     return (
@@ -36,6 +46,7 @@ export default function MainPage() {
             </form>
             {ingredients.length > 0 && 
                 <IngredientsList 
+                    ref={recipeSection}
                     ingredients={ingredients} //ingredients and toggleRecipeShown as props so they can be used in IngredientsList.jsx
                     getRecipe={getRecipe} />}
             {recipe && <HFRecipe recipe={recipe}/>} 
